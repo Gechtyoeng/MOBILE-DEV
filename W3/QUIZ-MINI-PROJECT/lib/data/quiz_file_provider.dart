f85cbd7dad03c196e6c0590981bd3ef155a0433d
OengGechty <gechtyoeng@gmail.com>
2025-10-24 10:15:11 +0700

    json better format

--- W3/QUIZ-MINI-PROJECT/lib/data/quiz_file_provider.dart
+++ W3/QUIZ-MINI-PROJECT/lib/data/quiz_file_provider.dart
@@ -11,18 +11,20 @@ class QuizRepository {
 //this function use to save quiz to json file
   Future<void> writeQuiz(Quiz quiz) async {
     final file = File(filePath);
-    final content = jsonEncode(quiz.toJson());
+    final encoder = JsonEncoder.withIndent('  ');//better format
+    //final content = jsonEncode(quiz.toJson());
+    String content = encoder.convert(quiz.toJson());
     await file.writeAsString(content);
   }
 
 //load the quiz from json file
-  Future<Quiz?> readQuiz() async {
+  Future<Quiz> readQuiz() async {
     final file = File(filePath);
-    if (!await file.exists()) return null; 
+    if (!await file.exists()) throw Exception('file path does not exist.');
 
     final content = await file.readAsString();
-    if (content.isEmpty) return null; // file is empty
-    
+    if (content.isEmpty) throw Exception('Content is empty.');
+
     final data = jsonDecode(content);
     return Quiz.fromJson(data);
   }
--- W3/QUIZ-MINI-PROJECT/lib/main.dart
+++ W3/QUIZ-MINI-PROJECT/lib/main.dart
@@ -4,18 +4,7 @@ import 'data/quiz_file_provider.dart';
 
 void main() async {
   final repo = QuizRepository(filePath: '../quiz_Data.json');
-  Quiz? quiz = await repo.readQuiz();
-
-if(quiz == null){
-  List<Question> questions = [
-  Question(title: "Capital of France?", choices: ["Paris", "London", "Rome"], goodChoice: "Paris", point: 5),
-  Question(title: "2 + 2 = ?", choices: ["2", "4", "5"], goodChoice: "4", point: 4),
-  ];
-
-  List<Player> players = [];
-
-  quiz = Quiz(questions: questions, players: players);
-}
+  Quiz quiz = await repo.readQuiz();
 
   QuizConsole console = QuizConsole(quiz: quiz);
 
